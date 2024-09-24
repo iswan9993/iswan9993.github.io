12b897ae50e9836ae102a6632984c1085e0bd32a
Ultraman <114324441+iswan9993@users.noreply.github.com>
2024-09-24 23:48:15 +0800

    posttest 3

---
+++ POSTTEST 3/script.js
@@ -0,0 +1,7 @@
+const menuIcon = document.getElementById("menu-icon-1");
+const menuList = document.getElementById("menu-list-1");
+
+menuIcon.addEventListener("click", () => {
+    menuList.classList.toggle("hidden-1");
+    
+});
